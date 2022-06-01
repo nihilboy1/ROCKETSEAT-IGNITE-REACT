90c78fc19032d0818aaca45e24750bfd6eb87a53
Samuel Seve <samuelseve1@gmail.com>
2022-06-01 16:22:12 -0300

    Progresso do cap 3

--- Chapter - III/aula/src/components/SubscribeButton/index.tsx
+++ Chapter - III/aula/src/components/SubscribeButton/index.tsx
@@ -1,5 +1,7 @@
 import { useSession, signIn } from 'next-auth/react'
 import S from './styles.module.scss'
+import { api } from '../../services/api'
+import { getStripeJs } from '../../services/stripe-js'
 
 interface SubscrbeButtonProps {
   priceId: string
@@ -8,18 +10,30 @@ interface SubscrbeButtonProps {
 export function SubscribeButton({ priceId }: SubscrbeButtonProps) {
   const { data: session } = useSession()
 
-
-
-  function handleSubscrbe(){
-    if (!session){
-      signIn("github")
+  async function handleSubscrbe() {
+    if (!session) {
+      signIn('github')
       return
     }
 
-    
+    try {
+      const res = await api.post('/subscribe')
+
+      const { sessionId } = res.data
+
+      const stripe = await getStripeJs()
+
+      await stripe.redirectToCheckout({sessionId})
+    } catch(error) {
+      alert(error.message)
+    }
   }
   return (
-    <button onClick={handleSubscrbe} type="button" className={S.subscribeButton}>
+    <button
+      onClick={handleSubscrbe}
+      type="button"
+      className={S.subscribeButton}
+    >
       Subscribe Now
     </button>
   )
