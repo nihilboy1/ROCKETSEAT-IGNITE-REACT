d663a7d2e4f645d4c811dd779c8e0df978fbd459
Samuel Seve <samuelseve1@gmail.com>
2022-08-09 06:48:50 -0300

    creating

--- Trilha 2021/Chapter - IV/aula2/src/contexts/AuthContext.tsx
+++ Trilha 2021/Chapter - IV/aula2/src/contexts/AuthContext.tsx
@@ -1,33 +1,50 @@
-import { createContext, ReactNode } from 'react'
+import { useRouter } from 'next/router'
+import { createContext, ReactNode, useState } from 'react'
+import { api } from '../services/api'
 
+type User = {
+  email: string
+  permissions: string[]
+  roles: string[]
+}
 interface SignInCredentials {
   email: string
   password: string
 }
-
 interface AuthContextData {
   // aqui ta retornando uma promise pq a função é assíncrona
   signIn(credentials: SignInCredentials): Promise<void>
+  user: User
   isAuthenticated: boolean
 }
-
 interface AuthProviderProps {
   children: ReactNode
 }
-
 export const AuthContext = createContext({} as AuthContextData)
 
+// ----------------------------------------------------------------
+
 export function AuthProvider({ children }: AuthProviderProps) {
-  const isAuthenticated = false
+  const router = useRouter()
+  const [user, setUser] = useState<User>()
+  const isAuthenticated = !!user
   async function signIn({ email, password }: SignInCredentials) {
-    console.log({
-      email,
-      password
-    })
+    try {
+      const response = await api.post('sessions', { email, password })
+      const { token, refreshtoken, permissions, roles } = response.data
+      setUser({
+        email,
+        permissions,
+        roles
+      })
+      router.push('/dashboard')
+    } catch (err) {
+      console.log(err)
+    }
   }
 
   return (
-    <AuthContext.Provider value={{ signIn, isAuthenticated }}>
+    <AuthContext.Provider value={{ signIn, isAuthenticated, user }}>
       {children}
     </AuthContext.Provider>
   )
