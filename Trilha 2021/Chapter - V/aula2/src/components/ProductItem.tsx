990c5301073562d90e6b79264702642c756fc044
Samuel Seve <samuelseve1@gmail.com>
2022-08-20 12:12:50 -0300

    memo function

--- Trilha 2021/Chapter - V/aula2/src/components/ProductItem.tsx
+++ Trilha 2021/Chapter - V/aula2/src/components/ProductItem.tsx
@@ -1,3 +1,5 @@
+import { memo } from 'react'
+
 interface ProductItemProps {
   product: {
     id: number
@@ -6,10 +8,45 @@ interface ProductItemProps {
   }
 }
 
-export function ProductItem({ product }: ProductItemProps) {
+function ProductItemComponent({ product }: ProductItemProps) {
   return (
     <div>
       {product.title} - <strong>{product.price}</strong>
     </div>
   )
 }
+
+export const ProductItem = memo(
+  ProductItemComponent,
+  (prevProps, nextProps) => {
+    return Object.is(prevProps.product, nextProps.product)
+  }
+)
+
+/* 
+                          //////////// shallow compare = comparação rasa /////////////
+
+Por padrão, sempre que há uma alteração no componente pai ou filho do componente X, 
+o componente X também é recriado. Contudo, usando o memo(), essa rerenderização do componente só será feita,
+caso haja alguma alteração de propriedade no próprio componente.
+
+
+
+
+                          //////////// deep compare = comparação profunda /////////////
+
+Contudo, quando se está lidando com comparação de objetos ou arrays
+(Tudo aquilo que o javascript não consegue fazer a comparação correta)
+o ideal é passar como segundo parametro para o memo(), uma função, que recebe como parametro o estado anterior e 
+o estado futuro das props em questão, para que eu possa fazer a comparação de um objeto que ela contenha. 
+Se houver alteração, ele atualiza o componente, se não, ele passa
+*/
+
+
+// Quando utilizar?
+
+/*
+1. Quando a funçaõ for "pura", apenas de layout
+2. Quando um componente estiver renderizando muitas vezes, com props iguais
+3. Quando a aplicação estiver com um tabalho de médio par grande
+*/
