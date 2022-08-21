e941032bf19b37b8f00bbd8e74ce472e0e659aa2
Samuel Seve <samuelseve1@gmail.com>
2022-08-20 22:06:19 -0300

    att

--- Trilha 2021/Chapter - V/aula2/src/components/SearchResults.tsx
+++ Trilha 2021/Chapter - V/aula2/src/components/SearchResults.tsx
@@ -1,3 +1,4 @@
+import { useMemo } from 'react'
 import { ProductItem } from './ProductItem'
 
 interface SearchResultsProps {
@@ -8,7 +9,27 @@ interface SearchResultsProps {
   }>
 }
 
+/* 
+o useMemo() evita que alguma tarefa pesada, de dentro do componente, seja refeita toda vez que o componente renderizar.
+
+// quando utilizar
+
+1. para calculos grandes
+2. quando eu preciso passar uma variavel que foi criada no componente para, para o componente filho
+(usando o useMemo nessa segunda situação, eu evito que o react tente comparar essas duas variaveis que são iguais, mas que são diferentes pelo aspecto referencial, de espaço na memória)
+*/
+
+
+
+
+
+
 export function SearchResults({ results }: SearchResultsProps) {
+  const totalPrice = useMemo(() => {
+    return results.reduce((total, product) => {
+      return total + product.price
+    }, 0)
+  }, [results])
   return (
     <div>
       {results.map(product => {
