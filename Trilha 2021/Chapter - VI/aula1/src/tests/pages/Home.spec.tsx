986c6f6f97cba955f37b34889714e53b430f3ed6
Samuel Seve <samuelseve1@gmail.com>
2022-08-24 17:18:00 -0300

    test

--- Trilha 2021/Chapter - VI/aula1/src/tests/pages/Home.spec.tsx
+++ Trilha 2021/Chapter - VI/aula1/src/tests/pages/Home.spec.tsx
@@ -1,5 +1,5 @@
 import { render, screen } from '@testing-library/react'
-import { debug } from 'console'
+import {stripe} from "../../services/stripe"
 import Home from '../../pages'
 
 jest.mock('next/router')
@@ -9,10 +9,18 @@ jest.mock('next-auth/react', () => {
   }
 })
 
+jest.mock("../../services/stripe")
+
 describe('Home page', () => {
   it('should be able to render correctly', () => {
     render(<Home product={{ priceId: 'fake-price-id', amount: 'R$10,00' }} />)
+
+    expect(screen.getByText('for R$10,00 month')).toBeInTheDocument()
   })
 
-  expect(screen.getByText('for R$10,00 month')).toBeInTheDocument()
+  it("should be able to load the initial data", () => {
+    const retrieveStripePricesMocked = jest.mocked(stripe.prices.retrieve)
+
+    retrieveStripePricesMocked
+  })
 })
